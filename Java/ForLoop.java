062f0a192463b3fe213b6775877792f8a4944344
Pratik Singh Rajput <spratik887@gmail.com>
2026-06-30 22:52:21 +0530

    Loops running

---
+++ Java/ForLoop.java
@@ -0,0 +1,11 @@
+/**
+ * ForLoop
+ */
+public class ForLoop {
+
+    public static void main(String[] args) {
+        for(int i=1; i<=100; i++){
+            System.out.println(i);
+        }
+    }
+}
